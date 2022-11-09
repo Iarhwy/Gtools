bd2f15c4e07d6ad3e452559a8cf23f906933660e
Yaroslav <109588856+Iarhwy@users.noreply.github.com>
2022-11-09 16:22:20 +0300

    Delete src directory

--- src/js/_vendor.js
+++
@@ -1,7 +0,0 @@
-import './vendor/just-validate3.3.3.min.js';
-import './vendor/validation.js';
-import './vendor/burger.js';
-import './vendor/login-modal.js';
-import './vendor/scroll-anim.js';
-import './vendor/scroll.js';
-import './vendor/ptools-tabs.js'
--- src/js/app.js
+++
@@ -1,3 +0,0 @@
-import './_vendor.js';
-import vars from './_vars.js';
-// import './_components.js';
